5c7f07f1157c539cd4ce4848e6908f9fe2c5eba2
Mathieu Parent <math.parent@gmail.com>
2014-11-13 16:56:57 +0100

    Test for name

--- spec/unit/puppet/type/winhttp_proxy_spec.rb
+++ spec/unit/puppet/type/winhttp_proxy_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe Puppet::Type.type(:winhttp_proxy) do
   let :winhttp_proxy do
     Puppet::Type.type(:winhttp_proxy).new(:name => 'proxy', :http_proxy_server => 'proxy.example.org')
@@ -6,6 +8,12 @@ describe Puppet::Type.type(:winhttp_proxy) do
   it 'should accept https_proxy_server' do
     winhttp_proxy[:https_proxy_server] = 'proxy.example.com'
     expect(winhttp_proxy[:https_proxy_server]).to eq('proxy.example.com')
+  end
+  it 'should not accept a name different than proxy' do
+    expect {
+      Puppet::Type.type(:winhttp_proxy).new(
+          :name   => 'something else'
+    )}.to raise_error(Puppet::Error, /Invalid value "something else". Valid values are proxy./)
   end
 end
 
