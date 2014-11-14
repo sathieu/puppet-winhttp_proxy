92e80ce14a4c5a3a37ab055a47efc9e2d9f4fa43
Mathieu Parent <Mathieu.PARENT@nantesmetropole.fr>
2014-11-14 15:24:37 +0100

    Warning

--- lib/puppet/provider/winhttp_proxy/netsh.rb
+++ lib/puppet/provider/winhttp_proxy/netsh.rb
@@ -56,7 +56,7 @@ Puppet::Type.type(:winhttp_proxy).provide(:netsh) do
         instances << proxy
         next
       end
-      Puppet.warning('Unable to parse line %s' % line)
+      warning("Unable to parse line #{line}")
     end
     instances
   end
